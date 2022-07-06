2aa19d5139f9a3e9dbed6cd0b49568e1f198641e
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-07-06 20:30:15 +0200

    Fix issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Vita-schedule'`, () => {
+  it(`should have as title 'KCorp-schedule'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Vita-schedule');
+    expect(app.title).toEqual('KCorp-schedule');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Vita-schedule app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('KCorp-schedule app is running!');
   });
 });
